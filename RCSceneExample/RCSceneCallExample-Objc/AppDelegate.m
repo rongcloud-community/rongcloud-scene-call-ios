bc51bef8db58ec3be5766d70f4637ea4ee4a7d1a
xiaoqing91 <hanxiaoiqng@rongcloud.cn>
2022-05-09 17:45:50 +0800

    finished
    Signed-off-by: xiaoqing91 <hanxiaoiqng@rongcloud.cn>

---
+++ RCSceneExample/RCSceneCallExample-Objc/AppDelegate.m
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.m
+//  RCSceneCallExample-Objc
+//
+//  Created by johankoi on 2022/5/9.
+//
+
+#import "AppDelegate.h"
+#import "RCSceneCallExample_Objc-Swift.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [AppConfigs config];
+    return YES;
+}
+
+
+@end
---
+++ RCSceneExample/RCSceneCallExample-Objc/ViewController.m
@@ -0,0 +1,67 @@
+//
+//  ViewController.m
+//  RCSceneCallExample-Objc
+//
+//  Created by johankoi on 2022/5/9.
+//
+
+#import "ViewController.h"
+#import "RCSceneCallExample_Objc-Swift.h"
+#import <SVProgressHUD/SVProgressHUD.h>
+#import <RCSceneCall.h>
+#import <RongIMKit/RCIM.h>
+//@import RongIMKit.RCIM;
+
+@interface ViewController ()
+
+@property (strong, nonatomic) SceneCallBridge *sceneCallBridge;
+
+
+@end
+
+@implementation ViewController
+
+- (SceneCallBridge *)sceneCallBridge {
+    if (!_sceneCallBridge) {
+        _sceneCallBridge = [[SceneCallBridge alloc] init];
+    }
+    return _sceneCallBridge;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"RCSceneCall";
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBar.hidden = NO;
+    [self connection];
+}
+
+
+- (IBAction)show:(id)sender {
+    [self.sceneCallBridge showDialVcFromVc:self];
+}
+- (void)connection {
+    if ([[RCIM sharedRCIM] getConnectionStatus] == ConnectionStatus_Connected) {
+        return;
+    }
+    NSString *rcToken = [self.sceneCallBridge userDefaultsSavedToken];
+    if (rcToken.length == 0) {
+        [self performSegueWithIdentifier:@"Login" sender:nil];
+        return;
+    }
+    [[RCIM sharedRCIM] initWithAppKey:[AppConfigs getRCKey]];
+    [[RCIM sharedRCIM] connectWithToken:rcToken dbOpened:^(RCDBErrorCode code) {
+        NSLog(@"RCIM db open failed: %zd",code);
+    } success:^(NSString *userId) {
+        NSLog(@"userId: %@",userId);
+         
+    } error:^(RCConnectErrorCode errorCode) {
+        NSLog(@"RCIM connect failed: %zd", errorCode);
+    }];
+}
+
+@end
